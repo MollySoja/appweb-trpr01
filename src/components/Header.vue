361465f3a330d1021ede7bb57f8801b25a043098
MollySoja <lorikparent@gmail.com>
2025-03-07 14:17:38 -0500

    deployed again

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@ const emit = defineEmits(["toggle-view"]);
 <template>
   <header class="bg-dark text-white py-2">
     <div class="container d-flex justify-content-center align-items-center">
-      <img src="../assets/ac_logo.webp" alt="ac_logo" width="50" class="me-4" />
+      <img src="../assets/ac_logo.png" alt="ac_logo" width="50" class="me-4" />
       <h1 class="h3">Magasin de Jeux Assassin's Creed</h1>
     </div>
   </header>
